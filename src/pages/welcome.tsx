2fb320fef36ca9b0029e4ed4762b4bb65ff40327
frodo821 <sakaic2003@gmail.com>
2020-12-24 18:50:56 +0900

    refresh everything

--- src/pages/welcome.tsx
+++
@@ -1,5 +0,0 @@
-import { h, FunctionComponent as FC } from 'preact';
-
-const Welcome: FC = () => <h1>It works!</h1>;
-
-export default Welcome;
--- tests/index.test.ts
+++
@@ -1,5 +0,0 @@
-describe('example test task', () => {
-  test('1 + 4 sould return 5', () => {
-    expect(1 + 4).toBe(5);
-  });
-});
